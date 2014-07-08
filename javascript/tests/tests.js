93ea4b9d85959ab6c02e2a44b3593227465c4674
Marko Samastur <markos@gaivo.net>
2014-07-08 20:37:36 +0200

    Another test for BaseView

--- javascript/tests/tests.js
+++ javascript/tests/tests.js
@@ -15,7 +15,25 @@ define(['views/base_view'], function (baseView) {
             equal(tv.template_selector, "#milestone-table-heading", "Correct selector");
 
             tv.render();
-            equal(tv.el.firstChild.innerHTML, "Test");
+            equal(tv.el.firstChild.innerHTML, "Test",
+                  "Rendered element contains model's data");
+        });
+
+        test("Test getTemplateData", function () {
+            var test_model = new Backbone.Model({name: "Test"}),
+                TestView = baseView.extend({
+                    template_selector: "#milestone-table-heading",
+                    el: "#qunit-fixture",
+                    getTemplateData: function (data) {
+                        data.name = "Changed test";
+                        return data;
+                    }
+                }), tv;
+
+            tv = new TestView({model: test_model});
+            tv.render();
+            equal(tv.el.firstChild.innerHTML, "Changed test",
+                  "Rendered element contains getTemplateData changes");
         });
 
     };
