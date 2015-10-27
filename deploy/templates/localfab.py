03e4fad73aa85afc1eab3323ec25ec4680922463
Alice Heaton <aliceh@aptivate.org>
2015-10-27 15:54:56 +0000

    Add templates needed to create new kashana instances

---
+++ deploy/templates/localfab.py
@@ -0,0 +1,86 @@
+from os import path
+from datetime import datetime
+from fabric.context_managers import settings
+from fabric.api import env, require
+import fablib
+from dye.fabfile import _server_setup
+
+
+def deploy(revision=None, keep=None, full_rebuild=True):
+    """ update remote host environment (virtualenv, deploy, update)
+
+    It takes three arguments:
+
+    * revision is the VCS revision ID to checkout (if not specified then
+      the latest will be checked out)
+    * keep is the number of old versions to keep around for rollback (default
+      5)
+    * full_rebuild is whether to do a full rebuild of the virtualenv
+    """
+    require('server_project_home', provided_by=env.valid_envs)
+
+    # this really needs to be first - other things assume the directory exists
+    fablib._create_dir_if_not_exists(env.server_project_home)
+
+    # if the <server_project_home>/previous/ directory doesn't exist, this does
+    # nothing
+    fablib._migrate_directory_structure()
+    fablib._set_vcs_root_dir_timestamp()
+
+    fablib.check_for_local_changes(revision)
+    # TODO: check for deploy-in-progress.json file
+    # also check if there are any directories newer than current ???
+    # might just mean we did a rollback, so maybe don't bother as the
+    # deploy-in-progress should be enough
+    # _check_for_deploy_in_progress()
+
+    # TODO: create deploy-in-progress.json file
+    # _set_deploy_in_progress()
+    fablib.create_copy_for_next()
+    fablib.checkout_or_update(in_next=True, revision=revision)
+    # remove any old pyc files - essential if the .py file is removed by VCS
+    if env.project_type == "django":
+        fablib.rm_pyc_files(path.join(env.next_dir, env.relative_django_dir))
+    # create the deploy virtualenv if we use it
+    fablib.create_deploy_virtualenv(in_next=True, full_rebuild=full_rebuild)
+
+    # we only have to disable this site after creating the rollback copy
+    # (do this so that apache carries on serving other sites on this server
+    # and the maintenance page for this vhost)
+    downtime_start = datetime.now()
+    fablib.link_webserver_conf(maintenance=True)
+    with settings(warn_only=True):
+        fablib.webserver_cmd('reload')
+    # TODO: do a database dump in the old directory
+    fablib.point_current_to_next()
+
+    # Use tasks.py deploy:env to actually do the deployment, including
+    # creating the virtualenv if it thinks it necessary, ignoring
+    # env.use_virtualenv as tasks.py knows nothing about it.
+    fablib._tasks('deploy:' + env.environment)
+
+    ensure_webassets_cache_writeable_by_apache()
+    # bring this vhost back in, reload the webserver and touch the WSGI
+    # handler (which reloads the wsgi app)
+    fablib.link_webserver_conf()
+    fablib.webserver_cmd('reload')
+    downtime_end = datetime.now()
+    fablib.touch_wsgi()
+
+    fablib.delete_old_rollback_versions(keep)
+    if env.environment == 'production':
+        fablib.setup_db_dumps()
+
+    # TODO: _remove_deploy_in_progress()
+    # move the deploy-in-progress.json file into the old directory as
+    # deploy-details.json
+    fablib._report_downtime(downtime_start, downtime_end)
+
+
+def ensure_webassets_cache_writeable_by_apache():
+    webassets_cache = path.join(fablib.env.django_dir, 'static')
+    fablib.sudo_or_run('chown -R apache %s' % webassets_cache)
+
+def {{instance}}():
+    """Run {{instance}} kashana on shared host"""
+    _server_setup('{{instance}}')
---
+++ deploy/templates/project_settings.py
@@ -0,0 +1,94 @@
+# this is for settings to be used by tasks.py
+import os
+from os import path
+
+###############################
+# THESE SETTINGS MUST BE EDITED
+###############################
+
+# This is the directory inside the project dev dir that contains the django
+# application
+project_name = "kashana"
+user = "aptivate"
+
+# The django apps that are part of this project - used for running tests
+# and migrations
+django_apps = ['appconf', 'contacts', 'dashboard', 'logframe']
+
+# repository type can be "cvs", "svn" or "git"
+repo_type = "git"
+repository = 'git@github.com:' + user + '/' + project_name + '.git'
+
+##################################################################
+# THESE SETTINGS MAY WELL BE CORRECT FOR A STANDARD DJANGO PROJECT
+# BUT SHOULD STILL BE REVIEWED
+##################################################################
+
+# put "django" here if you want django specific stuff to run
+# put "plain" here for a basic apache app
+project_type = "django"
+
+# does this virtualenv for python packages
+use_virtualenv = True
+
+################################
+# PATHS TO IMPORTANT DIRECTORIES
+################################
+
+# set the deploy directory to be the one containing this file
+local_deploy_dir = path.dirname(__file__)
+
+local_vcs_root = path.abspath(path.join(local_deploy_dir, os.pardir, os.pardir))
+
+# the path from the VCS root to the django root dir
+relative_django_dir = path.join('django', 'website')
+
+# the directory the settings live in, relative to the vcs root
+relative_django_settings_dir = relative_django_dir
+
+# the path from the VCS root to the virtualenv dir
+relative_ve_dir = path.join(relative_django_dir, '.ve')
+
+# requirements can be in a single file, or in a directory
+# the requirements file
+requirements_per_env = False
+local_requirements_file = path.join(local_deploy_dir, 'pip_packages.txt')
+
+# the requirements directory
+#requirements_per_env = True
+#local_requirements_dir = path.join(local_deploy_dir, 'requirements')
+# and the files should be path.join(requirements_dir, '%s.txt' % environment)
+
+test_cmd = ' manage.py test -v0 ' + ' '.join(django_apps)
+
+# servers, for use by fabric
+
+# production server - if commented out then the production task will abort
+host_list = {
+    '{{instance}}':['{{host}}'],
+}
+
+# this is the default git branch to use on each server
+default_branch = {
+    '{{instance}}':'{{branch}}',
+}
+
+# where on the server the django apps are deployed
+server_home = '/var/django'
+
+# the top level directory on the server
+# underneath it there will be dev/ containing the live instance
+# and previous/ containing old copies for rollback
+server_project_home = path.join(server_home, '{{instance}}.kashana')
+
+# which web server to use (or None)
+webserver = 'apache'
+
+###################################################
+# OPTIONAL SETTINGS FOR FABRIC - will be put in env
+###################################################
+
+# if you have an ssh key and particular user you need to use
+# then uncomment the next 2 lines
+#user = "root"
+#key_filename = ["/home/shared/keypair.rsa"]
