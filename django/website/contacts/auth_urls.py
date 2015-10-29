13238332442a802ab96f9dd53f0ba23c5fd6e33f
Daniel Levy <daniell@aptivate.org>
2015-10-29 14:47:47 +0000

    Update account activation

--- django/website/contacts/auth_urls.py
+++ django/website/contacts/auth_urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.views import (
-    login, logout_then_login, password_reset_confirm_uidb36
+    login, logout_then_login, password_reset_confirm
 )
 from .views import ResetPassword, change_password
 
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
 
     # Activation and password reset
     url(r'password_reset/$', ResetPassword.as_view(), name='password_reset'),
-    url(r'password_reset_confirm/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        password_reset_confirm_uidb36, {'post_reset_redirect': '/'}, name='password_reset_confirm'),
+    url(r'password_reset_confirm/(?P<uidb64>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        password_reset_confirm, {'post_reset_redirect': '/'}, name='password_reset_confirm'),
     url(r'password_change/$', change_password, name='password_change'),
 )
