3ed19b2672738a59fc8676e0403ee90fe57273a1
Daniel Levy <daniell@aptivate.org>
2015-11-05 15:50:59 +0000

    Add test for RatingAdmin colored_name

--- django/website/logframe/tests/test_admin.py
+++ django/website/logframe/tests/test_admin.py
@@ -1,7 +1,7 @@
 from mock import Mock
 
-from ..admin import SubIndicatorAdmin
-from ..models import SubIndicator
+from ..admin import RatingAdmin, SubIndicatorAdmin
+from ..models import colors, Rating, SubIndicator
 
 
 def test_sub_indicator_admin_rsult_returns_indicator_result():
@@ -9,3 +9,10 @@ def test_sub_indicator_admin_rsult_returns_indicator_result():
 
     admin = SubIndicatorAdmin(SubIndicator, None)
     assert sub_indicator.indicator.result == admin.result(sub_indicator)
+
+
+def test_rating_admin_colored_name_returns_name_for_colours():
+    obj = Mock(color=colors[0][0])
+
+    admin = RatingAdmin(Rating, None)
+    assert '<span class="rating-list-item {0}">{1}</span>'.format(colors[0][0], colors[0][1]) == admin.colored_name(obj)
