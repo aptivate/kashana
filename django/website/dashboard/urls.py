d6ae58d6127acd15475d0c2c70c155af045178d2
Daniel Levy <daniell@aptivate.org>
2015-12-15 12:26:43 +0000

    Switch urls around
    The '/dashboard/' view is now the one to select a log frame. The view to
    display data for a logframe is at '/dashboard/<logframe_name>/'.

--- django/website/dashboard/urls.py
+++ django/website/dashboard/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url
-from django.views.generic.base import View
 from .views import (
-    Home, DashboardView, SwitchLogframes
+    Home, DashboardView, DashboardLogframeSelection, SwitchLogframes
 )
 
 urlpatterns = [
-    url(r'^dashboard/$', DashboardView.as_view(), name='dashboard'),
+    url(r'^dashboard/$', DashboardLogframeSelection.as_view(), name='dashboard'),
     url(r'logframes/switch$', SwitchLogframes.as_view(), name='switch-logframes'),
-    url(r'^dashboard/(?P<slug>[\w_-]+)', View.as_view(), name='logframe-dashboard'),
+    url(r'^dashboard/(?P<slug>[\w\d_-]+)/$', DashboardView.as_view(), name='logframe-dashboard'),
     url(r'', Home.as_view(), name='home'),
 ]
