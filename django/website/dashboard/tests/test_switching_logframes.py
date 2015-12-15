d89f26432603e58b52db5d18585e58e42494b7d2
Daniel Levy <daniell@aptivate.org>
2015-12-15 15:08:11 +0000

    Rename logframe select variable

--- django/website/dashboard/tests/test_switching_logframes.py
+++ django/website/dashboard/tests/test_switching_logframes.py
@@ -9,8 +9,20 @@ from ..views import SwitchLogframes
 
 
 @mock.patch('dashboard.views.get_object_or_404', new=mock.Mock(return_value=mock.Mock(slug='test')))
+def test_switch_logframes_sets_session_id_to_new_logframe_id():
+    data = {'logframe': '2'}
+
+    request = RequestFactory().post('/', data)
+    request.user = mock.Mock()
+    request.session = mock.MagicMock(__setitem__=mock.Mock())
+    SwitchLogframes.as_view()(request)
+
+    request.session.__setitem__.assert_called_with('current_logframe', 'test')
+
+
+@mock.patch('dashboard.views.get_object_or_404', new=mock.Mock(return_value=mock.Mock(slug='test')))
 def test_switch_logframes_containts_instruction_to_redirect_to_dashboard():
-    data = {'id': '2'}
+    data = {'logframe': '2'}
 
     request = RequestFactory().post('/', data)
     request.user = mock.Mock()
@@ -22,7 +34,7 @@ def test_switch_logframes_containts_instruction_to_redirect_to_dashboard():
 
 @pytest.mark.django_db
 def test_switch_logframe_with_invalid_id_raises_404():
-    data = {'id': '-1'}
+    data = {'logframe': '-1'}
 
     request = RequestFactory().post('/', data)
     request.user = mock.Mock()
