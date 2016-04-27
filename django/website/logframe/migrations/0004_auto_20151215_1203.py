c6b9e070ff8971361f1e0206a26953267f7edf95
Daniel Levy <daniell@aptivate.org>
2016-04-27 09:43:24 +0100

    Fix bug with migration

--- django/website/logframe/migrations/0004_auto_20151215_1203.py
+++ django/website/logframe/migrations/0004_auto_20151215_1203.py
@@ -10,9 +10,9 @@ def set_logframe_slug(apps, schema_editor):
     LogFrame = apps.get_model('logframe', 'LogFrame')
     for logframe in LogFrame.objects.all():
         slug = logframe.name.lower()
-        slug.replace(' ', '_')
+        slug = slug.replace(' ', '_')
         slug = re.sub('[^\w-]', '', slug)
-        slug = slug[:47]
+        slug = slug[:46]
         if LogFrame.objects.filter(slug=slug).exists():
             count = LogFrame.objects.filter(slug__startswith=slug).count() + 1
             slug += unicode(count)
