234ac4a45efd5c425ff367e5fea1716f37edcf54
Daniel Levy <daniell@aptivate.org>
2017-03-01 16:45:44 +0000

    Amend migration to work with empty database

--- django/website/contacts/migrations/0007_assign_default_orgranisations.py
+++ django/website/contacts/migrations/0007_assign_default_orgranisations.py
@@ -9,19 +9,24 @@ def set_default_organisation_owner(apps, schema_editor):
     OrganizationOwner = apps.get_model('organizations', 'OrganizationOwner')
     OrganizationUser = apps.get_model('organizations', 'OrganizationUser')
     User = apps.get_model('contacts', 'User')
-    default_org = Organization.objects.get(name='Default Org')
-    user = User.objects.order_by('id')[0]
-    org_user = OrganizationUser.objects.get(user=user, organization=default_org)
-    OrganizationOwner.objects.create(organization_user=org_user, organization=default_org)
+    if User.objects.exists():
+        default_org = Organization.objects.get(name='Default Org')
+        user = User.objects.order_by('id')[0]
+        org_user = OrganizationUser.objects.get(user=user, organization=default_org)
+        OrganizationOwner.objects.create(organization_user=org_user, organization=default_org)
 
 
 def add_existing_users_to_default_organization(apps, schema_editor):
     Organization = apps.get_model('organizations', 'Organization')
     OrganizationUser = apps.get_model('organizations', 'OrganizationUser')
     User = apps.get_model('contacts', 'User')
-    default_org = Organization.objects.get(name='Default Org')
-    for user in User.objects.all():
-        OrganizationUser.objects.create(user=user, organization=default_org)
+    if Organization.objects.exists():
+        default_org = Organization.objects.get(name='Default Org')
+    else:
+        default_org = False
+    if default_org:
+        for user in User.objects.all():
+            OrganizationUser.objects.create(user=user, organization=default_org)
 
 
 class Migration(migrations.Migration):
