3a97f815802c94b695e9fdf99a0417b7b1fba77f
Daniel Levy <daniell@aptivate.org>
2017-03-06 12:26:49 +0000

    Don't allow names containing only tabs or spaces

--- django/website/logframe/forms.py
+++ django/website/logframe/forms.py
@@ -11,4 +11,6 @@ class CreateLogFrameForm(ModelForm):
     def clean_name(self):
         if LogFrame.objects.filter(name=self.cleaned_data['name']).exists():
             raise ValidationError("A Logframe with this name already exists")
+        if not self.cleaned_data['name'].strip():
+            raise ValidationError("The Logframe name must contain non-whitespace characters")
         return self.cleaned_data['name']
--- django/website/logframe/tests/test_forms.py
+++ django/website/logframe/tests/test_forms.py
@@ -6,9 +6,25 @@ from ..models import LogFrame
 
 
 @pytest.mark.django_db
-def test_logframe_name_validation_error_message():
+def test_logframe_with_duplication_name_raises_validation_error_message():
     G(LogFrame, name='test')
     form = CreateLogFrameForm(data={'name': 'test'})
     form.is_valid()
 
     assert ['A Logframe with this name already exists'] == form.errors['name']
+
+
+@pytest.mark.django_db
+def test_logframe_name_with_only_spaces_raises_validation_error_message():
+    form = CreateLogFrameForm(data={'name': ' '})
+    form.is_valid()
+
+    assert ['The Logframe name must contain non-whitespace characters'] == form.errors['name']
+
+
+@pytest.mark.django_db
+def test_logframe_name_with_only_tabs_raises_validation_error_message():
+    form = CreateLogFrameForm(data={'name': '\t'})
+    form.is_valid()
+
+    assert ['The Logframe name must contain non-whitespace characters'] == form.errors['name']
