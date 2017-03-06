3f8f6a125bfd04b79bef6dd26e7aa49fbc960c4e
Daniel Levy <daniell@aptivate.org>
2017-03-06 12:04:41 +0000

    Add view for selecting organizations

--- django/website/dashboard/urls.py
+++ django/website/dashboard/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from .views import (
-    Home, DashboardView, DashboardLogframeSelection, SwitchLogframes
+    Home, DashboardView, DashboardLogframeSelection,
+    DashboardOrganizationSelection, SwitchLogframes
 )
 
 urlpatterns = [
-    url(r'^dashboard/$', DashboardLogframeSelection.as_view(), name='dashboard'),
+    url(r'^dashboard/$', DashboardOrganizationSelection.as_view(), name='dashboard'),
+    url(r'^dashboard/(?P<org_slug>[\w\d_-]+)/$', DashboardLogframeSelection.as_view(), name='org-dashboard'),
     url(r'logframes/switch$', SwitchLogframes.as_view(), name='switch-logframes'),
     url(r'^dashboard/(?P<org_slug>[\w\d_-]+)/(?P<slug>[\w\d_-]+)/$', DashboardView.as_view(), name='logframe-dashboard'),
     url(r'', Home.as_view(), name='home'),
--- django/website/dashboard/views.py
+++ django/website/dashboard/views.py
@@ -4,6 +4,7 @@ from django.views.generic.base import RedirectView
 from django.views.generic.list import ListView
 
 from braces.views import LoginRequiredMixin
+from organizations.models import Organization
 
 from logframe.mixins import AptivateDataBaseMixin
 from logframe.models import LogFrame
@@ -27,10 +28,19 @@ class SwitchLogframes(LoginRequiredMixin, RedirectView):
     def post(self, request, *args, **kwargs):
         self.object = get_object_or_404(LogFrame, pk=self.request.POST['logframe'])
         update_last_viewed_item(self.request.user, self.object)
-        return self.get(request, slug=self.object.slug)
+        return self.get(request, org_slug=self.object.organization.slug, slug=self.object.slug)
+
+
+class DashboardOrganizationSelection(LoginRequiredMixin, ListView):
+    model = Organization
+    context_object_name = 'organization_list'
+    template_name = 'dashboard/dashboard_organization_list.html'
 
 
 class DashboardLogframeSelection(LoginRequiredMixin, ListView):
     model = LogFrame
     context_object_name = 'logframe_list'
     template_name = 'dashboard/dashboard_logframe_list.html'
+
+    def get_queryset(self):
+        return self.model.objects.filter(organization__slug=self.kwargs.get('org_slug'))
