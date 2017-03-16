f29383f1f11932b590efb859f73cc8a1c3dba6e3
Daniel Levy <daniell@aptivate.org>
2017-03-16 11:08:28 +0000

    Remove debugging info

--- django/website/custom_organizations/mixins.py
+++ django/website/custom_organizations/mixins.py
@@ -6,6 +6,5 @@ class GetOrgBySlugMixin(object):
 
     def get_object(self):
         model = self.get_org_model()
-        print self.kwargs[self.slug_field]
         return get_object_or_404(model, slug=self.kwargs[self.slug_field])
     get_organization = get_object
