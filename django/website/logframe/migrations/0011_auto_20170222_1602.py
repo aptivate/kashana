5bec20f71c2d291ee3297eee9f732dc17337e09f
Daniel Levy <daniell@aptivate.org>
2017-03-17 17:20:30 +0000

    Update migration to use default organization name

--- django/website/logframe/migrations/0011_auto_20170222_1602.py
+++ django/website/logframe/migrations/0011_auto_20170222_1602.py
@@ -2,13 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import settings
 
 
 def create_default_logframe(apps, schema_editor):
     LogFrame = apps.get_model('logframe', 'LogFrame')
     Organization = apps.get_model('organizations', 'Organization')
     if LogFrame.objects.exists():
-        default_org = Organization.objects.create(name='Default Org')
+        default_org = Organization.objects.create(name=settings.DEFAULT_ORGANIZATION_NAME)
         for logframe in LogFrame.objects.all():
             logframe.organization = default_org
             logframe.save()
